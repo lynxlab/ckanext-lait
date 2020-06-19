ffa6fd2f4a126ef0c1a875a5556be303ddeb6488
iXB3 <matteocausio@yahoo.it>
2020-06-19 18:26:00 +0200

    tolti facets cataloghi e regioni anche nella pagina di gruppi e organizzazioni

--- ckanext/lait/plugin.py
+++ ckanext/lait/plugin.py
@@ -207,9 +207,19 @@ class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
 	return facets_dict
 	
     def group_facets(self, facets_dict, group_type, package_type):
+	for facet_key in facets_dict.keys():
+	    if 'organization_region_' in facet_key:
+                del facets_dict[facet_key]
+        if 'source_catalog_title' in facets_dict.keys():
+            del facets_dict['source_catalog_title']
         return facets_dict
 		
     def organization_facets(self, facets_dict, organization_type, package_type):
+	for facet_key in facets_dict.keys():
+	    if 'organization_region_' in facet_key:
+                del facets_dict[facet_key]
+        if 'source_catalog_title' in facets_dict.keys():
+            del facets_dict['source_catalog_title']
         return facets_dict
 
     def before_show(self, data_dict):
