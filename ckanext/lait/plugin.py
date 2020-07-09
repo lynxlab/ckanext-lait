0e6ba7196f910c541d0fe02d4eed3b3ee4be825f
Giorgio Consorti <g.consorti@lynxlab.com>
2020-07-09 15:38:55 +0200

    add IPackageController.before_view implementation and comment not needed code

--- ckanext/lait/plugin.py
+++ ckanext/lait/plugin.py
@@ -27,165 +27,165 @@ def convert_string_to_dict(string):
 	# return ast.literal_eval(string)
 	return json.loads(string) if string else {}
 
-def infograph_data(res,infograph_config):
-	res_id = res['id']
-	axis1 = infograph_config['axis1']
-	axis2 = infograph_config['axis2']
-	aggregation = infograph_config.get('aggregation','sum')
-	# sort = infograph_config.get('sort','_id')
-	# order = infograph_config.get('order','ASC')
-	query = 'SELECT "'+axis1+'",'+aggregation+'("'+axis2+'") AS "'+axis2+'" FROM "'+res_id+'" WHERE "'+axis2+'" IS NOT NULL'
-	if infograph_config.get('filter',[]):
-		for f in infograph_config.get('filter',[]):
-			query += ' AND (1=0'
-			for v in f.get('values',[]):
-				query += ' OR "'+f['column']+'" '+f.get('operator','=')+' \''+v+'\''
-			query += ')'
-	query += ' GROUP BY "'+axis1+'" ORDER BY "'+axis1+'"'
-	# +' LIMIT 10'
-	log.debug(query)
-	url = config.get('ckan.base_url')+'/catalog/api/action/datastore_search_sql?sql='+query.replace(' ','%20')
-	try:
-		response = urllib2.urlopen(url)
-		response_body = response.read()
-	except urllib2.HTTPError as e:
-		error_message = json.loads(e.read())
-		return error_message
-	except Exception, inst:
-		msg = "Couldn't read response from datastore service %r: %s" % (response_body, inst)
-		raise Exception, inst
-	try:
-		datastore = json.loads(response_body)
-	except Exception, inst:
-		msg = "Couldn't read response from datastore service %r: %s" % (response_body, inst)
-		raise Exception, inst
-	data = []
-	for r in datastore["result"]["records"]:
-		data.append({'axis1':r[axis1],'axis2':float(r[axis2])})
-	return data
+# def infograph_data(res,infograph_config):
+# 	res_id = res['id']
+# 	axis1 = infograph_config['axis1']
+# 	axis2 = infograph_config['axis2']
+# 	aggregation = infograph_config.get('aggregation','sum')
+# 	# sort = infograph_config.get('sort','_id')
+# 	# order = infograph_config.get('order','ASC')
+# 	query = 'SELECT "'+axis1+'",'+aggregation+'("'+axis2+'") AS "'+axis2+'" FROM "'+res_id+'" WHERE "'+axis2+'" IS NOT NULL'
+# 	if infograph_config.get('filter',[]):
+# 		for f in infograph_config.get('filter',[]):
+# 			query += ' AND (1=0'
+# 			for v in f.get('values',[]):
+# 				query += ' OR "'+f['column']+'" '+f.get('operator','=')+' \''+v+'\''
+# 			query += ')'
+# 	query += ' GROUP BY "'+axis1+'" ORDER BY "'+axis1+'"'
+# 	# +' LIMIT 10'
+# 	log.debug(query)
+# 	url = config.get('ckan.base_url')+'/catalog/api/action/datastore_search_sql?sql='+query.replace(' ','%20')
+# 	try:
+# 		response = urllib2.urlopen(url)
+# 		response_body = response.read()
+# 	except urllib2.HTTPError as e:
+# 		error_message = json.loads(e.read())
+# 		return error_message
+# 	except Exception, inst:
+# 		msg = "Couldn't read response from datastore service %r: %s" % (response_body, inst)
+# 		raise Exception, inst
+# 	try:
+# 		datastore = json.loads(response_body)
+# 	except Exception, inst:
+# 		msg = "Couldn't read response from datastore service %r: %s" % (response_body, inst)
+# 		raise Exception, inst
+# 	data = []
+# 	for r in datastore["result"]["records"]:
+# 		data.append({'axis1':r[axis1],'axis2':float(r[axis2])})
+# 	return data
 
-def comuni():
-    response_body = ''
-    try:
-        comuni = json.loads(response_body)
-    except Exception, inst:
-        msg = "Couldn't read response from comuni service %r: %s" % (response_body, inst)
-        raise Exception, inst
-    return comuni
+# def comuni():
+#     response_body = ''
+#     try:
+#         comuni = json.loads(response_body)
+#     except Exception, inst:
+#         msg = "Couldn't read response from comuni service %r: %s" % (response_body, inst)
+#         raise Exception, inst
+#     return comuni
 
-def facets():
-    d = collections.OrderedDict()
-    d['organization'] = _('Organizations')
-    d['tags'] = _('Tags')
-    d['res_format'] = _('Formats')
-    d['license_id'] = _('Licenses')
-    return d
+# def facets():
+#     d = collections.OrderedDict()
+#     d['organization'] = _('Organizations')
+#     d['tags'] = _('Tags')
+#     d['res_format'] = _('Formats')
+#     d['license_id'] = _('Licenses')
+#     return d
 
-def translate_resource_data_dict(data_dict):
-    '''Return the given dict with as many of its fields
-    as possible translated into the desired or the fallback language.
+# def translate_resource_data_dict(data_dict):
+#     '''Return the given dict with as many of its fields
+#     as possible translated into the desired or the fallback language.
 
-    '''
-    try:
-        desired_lang_code = pylons.request.environ['CKAN_LANG']
-    except Exception, e:
-        desired_lang_code = 'it'
+#     '''
+#     try:
+#         desired_lang_code = pylons.request.environ['CKAN_LANG']
+#     except Exception, e:
+#         desired_lang_code = 'it'
 
-    fallback_lang_code = pylons.config.get('ckan.locale_default', 'en')
+#     fallback_lang_code = pylons.config.get('ckan.locale_default', 'en')
 
-    # Get a flattened copy of data_dict to do the translation on.
-    flattened = navl.dictization_functions.flatten_dict(data_dict)
+#     # Get a flattened copy of data_dict to do the translation on.
+#     flattened = navl.dictization_functions.flatten_dict(data_dict)
 
-    # Get a simple flat list of all the terms to be translated, from the
-    # flattened data dict.
-    terms = sets.Set()
-    for (key, value) in flattened.items():
-        if value in (None, True, False):
-            continue
-        elif isinstance(value, basestring):
-            terms.add(value)
-        elif isinstance(value, (int, long)):
-            continue
-        else:
-            for item in value:
-                 terms.add(item)
+#     # Get a simple flat list of all the terms to be translated, from the
+#     # flattened data dict.
+#     terms = sets.Set()
+#     for (key, value) in flattened.items():
+#         if value in (None, True, False):
+#             continue
+#         elif isinstance(value, basestring):
+#             terms.add(value)
+#         elif isinstance(value, (int, long)):
+#             continue
+#         else:
+#             for item in value:
+#                  terms.add(item)
 
-    # Get the translations of all the terms (as a list of dictionaries).
-    translations = ckan.logic.action.get.term_translation_show(
-            {'model': ckan.model},
-            {'terms': terms,
-                'lang_codes': (desired_lang_code, fallback_lang_code)})
-    # Transform the translations into a more convenient structure.
-    desired_translations = {}
-    fallback_translations = {}
-    for translation in translations:
-        if translation['lang_code'] == desired_lang_code:
-            desired_translations[translation['term']] = (
-                    translation['term_translation'])
-        else:
-            assert translation['lang_code'] == fallback_lang_code
-            fallback_translations[translation['term']] = (
-                    translation['term_translation'])
+#     # Get the translations of all the terms (as a list of dictionaries).
+#     translations = ckan.logic.action.get.term_translation_show(
+#             {'model': ckan.model},
+#             {'terms': terms,
+#                 'lang_codes': (desired_lang_code, fallback_lang_code)})
+#     # Transform the translations into a more convenient structure.
+#     desired_translations = {}
+#     fallback_translations = {}
+#     for translation in translations:
+#         if translation['lang_code'] == desired_lang_code:
+#             desired_translations[translation['term']] = (
+#                     translation['term_translation'])
+#         else:
+#             assert translation['lang_code'] == fallback_lang_code
+#             fallback_translations[translation['term']] = (
+#                     translation['term_translation'])
 
-    # Make a copy of the flattened data dict with all the terms replaced by
-    # their translations, where available.
-    translated_flattened = {}
-    for (key, value) in flattened.items():
+#     # Make a copy of the flattened data dict with all the terms replaced by
+#     # their translations, where available.
+#     translated_flattened = {}
+#     for (key, value) in flattened.items():
 
-        # Don't translate names that are used for form URLs.
-        if key == ('name',):
-            if value in desired_translations:
-                translated_flattened[key] = desired_translations[value]
-            elif value in fallback_translations:
-                translated_flattened[key] = fallback_translations.get(value, value)
-            else:
-                translated_flattened[key] = value
+#         # Don't translate names that are used for form URLs.
+#         if key == ('name',):
+#             if value in desired_translations:
+#                 translated_flattened[key] = desired_translations[value]
+#             elif value in fallback_translations:
+#                 translated_flattened[key] = fallback_translations.get(value, value)
+#             else:
+#                 translated_flattened[key] = value
 
-        elif value in (None, True, False):
-            # Don't try to translate values that aren't strings.
-            translated_flattened[key] = value
+#         elif value in (None, True, False):
+#             # Don't try to translate values that aren't strings.
+#             translated_flattened[key] = value
 
-        elif isinstance(value, basestring):
-            if value in desired_translations:
-                translated_flattened[key] = desired_translations[value]
-            else:
-                translated_flattened[key] = fallback_translations.get(
-                        value, value)
+#         elif isinstance(value, basestring):
+#             if value in desired_translations:
+#                 translated_flattened[key] = desired_translations[value]
+#             else:
+#                 translated_flattened[key] = fallback_translations.get(
+#                         value, value)
 
-        elif isinstance(value, (int, long, dict)):
-            translated_flattened[key] = value
+#         elif isinstance(value, (int, long, dict)):
+#             translated_flattened[key] = value
 
-        else:
-            translated_value = []
-            for item in value:
-                if item in desired_translations:
-                    translated_value.append(desired_translations[item])
-                else:
-                    translated_value.append(
-                        fallback_translations.get(item, item)
-                    )
-            translated_flattened[key] = translated_value
-    # Finally unflatten and return the translated data dict.
-    translated_data_dict = (navl.dictization_functions
-            .unflatten(translated_flattened))
-    return translated_data_dict
+#         else:
+#             translated_value = []
+#             for item in value:
+#                 if item in desired_translations:
+#                     translated_value.append(desired_translations[item])
+#                 else:
+#                     translated_value.append(
+#                         fallback_translations.get(item, item)
+#                     )
+#             translated_flattened[key] = translated_value
+#     # Finally unflatten and return the translated data dict.
+#     translated_data_dict = (navl.dictization_functions
+#             .unflatten(translated_flattened))
+#     return translated_data_dict
 
-def translate_resource_data_dict_list(data_dict_list):
-    translated = []
-    for dict in data_dict_list:
-        translated.append(translate_resource_data_dict(dict))
-    return translated
+# def translate_resource_data_dict_list(data_dict_list):
+#     translated = []
+#     for dict in data_dict_list:
+#         translated.append(translate_resource_data_dict(dict))
+#     return translated
 
-def translate_related_list(related_list):
-    translated = []
-    for related in related_list:
-        dict = {'title':related.title, 'description':related.description}
-        dict_trans = translate_resource_data_dict(dict)
-        dict_trans['id'] = related.id
-        dict_trans['type'] = related.type
-        dict_trans['url'] = related.url
-        translated.append(dict_trans)
-    return translated
+# def translate_related_list(related_list):
+#     translated = []
+#     for related in related_list:
+#         dict = {'title':related.title, 'description':related.description}
+#         dict_trans = translate_resource_data_dict(dict)
+#         dict_trans['id'] = related.id
+#         dict_trans['type'] = related.type
+#         dict_trans['url'] = related.url
+#         translated.append(dict_trans)
+#     return translated
 
 class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.IConfigurer)
@@ -194,6 +194,27 @@ class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.IResourceController, inherit=True)
     p.implements(p.IFacets)
     p.implements(p.ITranslation)
+    p.implements(p.IPackageController, inherit=True)
+
+    ## implement IPackageController.before_view
+    def before_view(self, pkg_dict):
+        groups = pkg_dict['groups']
+        if len(groups)>0:
+            context = {'model': ckan.model, 'session': ckan.model.Session,
+                       'user': c.user, 'for_view': True,
+                       'auth_user_obj': c.userobj, 'use_cache': False}
+            for idx, group in enumerate(groups):
+                data_dict = {'id': group['id']}
+                try:
+                    loadedGroup = ckan.logic.get_action('group_show')(context, data_dict)
+                    if 'users' in loadedGroup.keys():
+                        del loadedGroup['users']
+                    pkg_dict['groups'][idx] = loadedGroup
+                except:
+                    log.info('[before_view] Error loading group: ' + groups[idx]['name'])
+        else:
+            log.info('[before_view] No group for dataset: ' + pkg_dict['name'])
+        return pkg_dict
 
     def dataset_facets(self, facets_dict, package_type):
 	for facet_key in facets_dict.keys():
@@ -202,7 +223,7 @@ class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
         if 'source_catalog_title' in facets_dict.keys():
             del facets_dict['source_catalog_title']
 	return facets_dict
-	
+
     def group_facets(self, facets_dict, group_type, package_type):
 	for facet_key in facets_dict.keys():
 	    if 'organization_region_' in facet_key:
@@ -210,7 +231,7 @@ class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
         if 'source_catalog_title' in facets_dict.keys():
             del facets_dict['source_catalog_title']
         return facets_dict
-		
+
     def organization_facets(self, facets_dict, organization_type, package_type):
 	for facet_key in facets_dict.keys():
 	    if 'organization_region_' in facet_key:
@@ -219,15 +240,17 @@ class LaitPlugin(p.SingletonPlugin, DefaultTranslation):
             del facets_dict['source_catalog_title']
         return facets_dict
 
-    def before_show(self, data_dict):
-        translated_data_dict = translate_resource_data_dict(data_dict)
-        return translated_data_dict
+    # def before_show(self, data_dict):
+    #     translated_data_dict = translate_resource_data_dict(data_dict)
+    #     return translated_data_dict
 
     def get_helpers(self):
-        return {'translate_related_list': translate_related_list,
-		'translate_resource_data_dict_list': translate_resource_data_dict_list,
-		'comuni': comuni, 'facets':facets,
-		'infograph_data': infograph_data,
+        return {
+        # 'translate_related_list': translate_related_list,
+		# 'translate_resource_data_dict_list': translate_resource_data_dict_list,
+		# 'comuni': comuni,
+        # 'facets':facets,
+		# 'infograph_data': infograph_data,
 		'convert_string_to_dict': convert_string_to_dict,
 		'convert_dict_to_string': convert_dict_to_string}
 
